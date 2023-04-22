7ccfc4bc43f482fd55442113a6b2efb9f1260b99
David Dalpiaz <dalpiaz2@illinois.edu>
2023-04-21 22:53:42 -0500

    updates and notes about BA and other statistics

--- R/Statistics.R
+++ R/Statistics.R
@@ -1,11 +1,21 @@
-#' Calculate batting average
+#' Calculate batting average and add to data frame
 #'
 #' @param data A data frame with columns H (hits) and AB (at-bats)
 #'
-#' @return A data frame with the BA column
+#' @return The input data frame with added BA column
 #' @export
+#'
+#' @examples
+#' calcBA(Batting)
 calcBA = function(data) {
-  # TODO: verify input data is valid
+  if (!is.data.frame(data) || with(data, {!exists("H") || !exists("AB")})) {
+    stop("data must be a data frame with columns for H (hits) and AB (at-bats)")
+  }
+  # TODO: what if AB is 0? currently NaN? should it be NA?
   data$BA = data$H / data$AB
   return(data)
 }
+
+# TODO: instead of single functions with df as input could instead have a function
+# for each statistic that is called within an overall function that adds a bunch of
+# computed statistics. Could probably provide everything list on bref
